2db29eac3776f965300d84518a157cfd00346e32
TheTraveller95 <crivellaroandres@gmail.com>
2020-01-12 16:33:00 +0000

    add base.html and tasks.html templates

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_URI'] = 'mongodb+srv://root:r00tUser@myfirstcluster-p7dea.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template('tasks.html', tasks = mongo.db.tasks.find()) # el 'tasks.find()' es el method para buscar entre los records the nuestra collection en MongoDB llamada tasks
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
