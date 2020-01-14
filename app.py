2ec9b7d6017824e3d5db358329fcf70215cf2c28
TheTraveller95 <crivellaroandres@gmail.com>
2020-01-14 20:37:03 +0000

    add style.css and the adding task function

--- app.py
+++ app.py
@@ -18,7 +18,14 @@ def get_tasks():
 @app.route('/add_tasks')
 def add_tasks():
     return render_template('addtask.html', 
-    categories= mongo.db.categories.find())
+    categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    task = mongo.db.tasks
+    task.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
