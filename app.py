b0a0472f11895e0a821a6030cd1212b260ef9994
TheTraveller95 <crivellaroandres@gmail.com>
2020-01-15 19:47:09 +0000

    add the edittask.html template with the edit function

--- app.py
+++ app.py
@@ -27,5 +27,11 @@ def insert_task():
     task.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task, categories=all_categories)
+
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
